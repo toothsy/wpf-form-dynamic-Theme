4f15a8016857a4e85cb7d2c95082ce83eaf65cc6
toothsy <c.j.atharva@gmail.com>
2022-03-29 14:52:26 +0530

    intial commit

---
+++ Commands/UpdateView.cs
@@ -0,0 +1,42 @@
+﻿using formProj.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace formProj.Commands
+{
+    public class UpdateView : ICommand
+    {
+        private MainViewModel viewModel;
+        private bool visitedLogin = false;
+
+        public UpdateView(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            
+            if (!this.visitedLogin && parameter.ToString() == "login")
+            {
+                viewModel.SelectedViewModel = new LoginViewModel();
+                this.visitedLogin = true;
+            }
+
+            if (this.visitedLogin){
+
+            }
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using formProj.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel;
+namespace formProj.ViewModels
+{
+    public class MainViewModel : BaseViewModel , INotifyPropertyChanged
+    {
+        private BaseViewModel _selectedVM = new SignUpViewModel();
+        private string buttonContent = "Submit";
+        public string ButtonContent { get => buttonContent; set {  
+                buttonContent = value;
+                OnPropertyChanged(nameof(buttonContent));
+            } 
+        }
+
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedVM; }
+            set { _selectedVM = value;
+                this.ButtonContent = "Login" ;
+                OnPropertyChanged(nameof(SelectedViewModel));
+                
+               
+            }
+        }
+
+        public ICommand UpdateView { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateView = new UpdateView(this);
+        }
+
+    }
+}
